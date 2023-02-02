14973e65e51192f28fd93f53a18cab01d0c8f49a
filipton <filipton12@gmail.com>
2023-02-02 02:15:57 +0100

    feat: neovim builder

---
+++ builder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+sudo pacman --needed -S base-devel cmake unzip ninja tree-sitter curl
+
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_YPE=RelWithDebInfo
+make install
+
+mv ./build/bin/nvim /usr/bin/nvim
