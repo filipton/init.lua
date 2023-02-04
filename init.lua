b7ef18d0a69b9f340df5ce1262ceab095be18e09
filipton <filipton12@gmail.com>
2023-02-04 22:56:48 +0000

    feat: changed from packer to lazy.nvim

--- init.lua
+++ init.lua
@@ -1,1 +1,72 @@
 require("notpilif")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+    {
+        'nvim-telescope/telescope.nvim',
+        version = '0.1.1',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function ()
+            vim.cmd([[TSUpdate]])
+        end
+    },
+    {
+        'nvim-treesitter/playground'
+    },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+    },
+    {
+        'mbbill/undotree'
+    },
+    {
+        'tpope/vim-fugitive'
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            { 'williamboman/mason.nvim' }, -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    },
+    {
+        'theprimeagen/harpoon'
+    },
+    {
+        "folke/zen-mode.nvim"
+    },
+    {
+        "github/copilot.vim"
+    }
+})
