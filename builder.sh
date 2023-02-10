38339ec278030ee6e6da60b977f43974e7d32457
filipton <filipton12@gmail.com>
2023-02-11 00:50:12 +0100

    feat: builder.sh without interactivity

--- builder.sh
+++ builder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-sudo pacman --needed -S base-devel cmake unzip ninja tree-sitter curl
+sudo apt install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen 
+sudo pacman --needed --noconfirm -S base-devel cmake unzip ninja tree-sitter curl
 
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_YPE=RelWithDebInfo
