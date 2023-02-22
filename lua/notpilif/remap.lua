ca803a066dbd198432d585a66544e488db3e2321
filipton <filipton12@gmail.com>
2023-02-22 23:42:50 +0100

    feat: neotree remap

--- lua/notpilif/remap.lua
+++ lua/notpilif/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<C-n>", "<cmd>NeoTreeFocus<CR>")
+vim.keymap.set("n", "<C-n>", "<cmd>NeoTreeFocusToggle<CR>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
