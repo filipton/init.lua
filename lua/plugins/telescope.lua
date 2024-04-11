3568e5979c371d786d17ec676a8470cfbaae9acf
filipton <filipton12@gmail.com>
2024-04-11 20:18:07 +0200

    feat: lsp references (gr)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+        vim.keymap.set('n', 'gr', builtin.lsp_references, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
