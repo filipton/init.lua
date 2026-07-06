f77baf5820312a10a0f594e07cf59e1601cdaecf
filipton <filipton12@gmail.com>
2026-07-06 19:28:39 +0200

    feat: load .nvim.lua files

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -27,6 +27,9 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+-- Load per-project .nvim.lua files
+vim.o.exrc = true
+
 -- REMAPS
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
